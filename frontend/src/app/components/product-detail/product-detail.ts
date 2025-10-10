1695e0e5989e827508378269a104d0eaf54c0311
adamkosovic <adam.kosovic@icloud.com>
2025-10-10 15:47:21 +0200

    ProductDetail

--- frontend/src/app/components/product-detail/product-detail.ts
+++ frontend/src/app/components/product-detail/product-detail.ts
@@ -1,21 +1,37 @@
 import { Component,  OnInit} from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../../services/products';
+import { Product } from '../../models/product/product.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-detail.html',
   styleUrl: './product-detail.css'
 })
 export class ProductDetail implements OnInit {
-  productId! : string | null;
+  product?: Product;
 
-  constructor( private router: ActivatedRoute) { }
+  constructor( 
+    private route: ActivatedRoute,
+    private router: Router,
+    private productservice: ProductService,
+    ) {}
 
   ngOnInit() {
-    this.router.paramMap.subscribe(params => {
-      const productId = params.get('id');
-      // Use the productId to fetch product details from a service or API
-    });
+    const id = this.route.snapshot.paramMap.get('id');
+    if (!id) {
+      this.router.navigate(['/shop']);
+      return;
+    }
+
+    const found = this.productservice.getById(id);
+    if (!found) {
+      this.router.navigate(['/shop']);
+      return;
+    }
+
+    this.product = found;
   }
 }
--- frontend/src/app/components/product-list/product-list.ts
+++ frontend/src/app/components/product-list/product-list.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product/product.model';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { ProductService } from '../../services/products';
+
 
 
 @Component({
@@ -11,73 +13,22 @@ import { Router } from '@angular/router';
   templateUrl: './product-list.html',
   styleUrl: './product-list.css'
 })
-export class ProductList {
+export class ProductList implements OnInit {
+  products: Product[] = [];
 
-  constructor(private router: Router) {}
+  constructor (private productService: ProductService, private router: Router) {}
 
-  goToProductDetail(id: string) {
+  
+  ngOnInit() {
+    this.products = this.productService.getAll();
+  }
+  
+  goToProduct(id: string) {
     this.router.navigate(['/shop', id]);
   }
+  
 
-  trackByProduct = (index: number, product: Product ) => product.id;
-
-
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'Eco Tee',
-      price: 99,
-      imageUrl: 'https://picsum.photos/seed/eco-tee/400/300',
-      categories: ['men' , 'sale']
-    },
-    {
-      id: '2',
-      name: 'Classic Shirt',
-      price: 149,
-      imageUrl: 'https://picsum.photos/seed/classic-shirt/400/300',
-      categories: ['men', 'sale']
-    },
-    {
-      id: '3',
-      name: 'Classic Shirt',
-      price: 149,
-      imageUrl: 'https://picsum.photos/seed/linen-dress/400/300',
-      categories: ['men', 'sale']
-    },
-    {
-      id: '4',
-      name: 'Classic Shirt',
-      price: 149,
-      imageUrl: 'https://picsum.photos/seed/kids-hoodie/400/300',
-      categories: ['men', 'sale']
-    },
-    {
-      id: '5',
-      name: 'Classic Shirt',
-      price: 149,
-      imageUrl: 'https://picsum.photos/seed/kids-hoodie/400/300',
-      categories: ['men', 'sale']
-    },
-    {
-      id: '6',
-      name: 'Classic Shirt',
-      price: 149,
-      imageUrl: 'https://picsum.photos/seed/kids-hoodie/400/300',
-      categories: ['men', 'sale']
-    },
-    {
-      id: '7',
-      name: 'Classic Shirt',
-      price: 149,
-      imageUrl: 'https://picsum.photos/seed/kids-hoodie/400/300',
-      categories: ['men', 'sale']
-    },
-    {
-      id: '8',
-      name: 'Classic Shirt',
-      price: 149,
-      imageUrl: 'https://picsum.photos/seed/kids-hoodie/400/300',
-      categories: ['men', 'sale']
-    },
-  ]
+  trackByProductId(index: number, product: Product): string {
+    return product.id;
+  }
 }
