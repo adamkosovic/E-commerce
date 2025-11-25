82d64d5a8efba45065ddca501d16ac7d0e62a22b
adamkosovic <adam.kosovic@icloud.com>
2025-11-25 10:51:19 +0100

    Login

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../auth/auth.service';
+import { Router } from "@angular/router";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  email: string = '';
+  password: string = '';
+  errorMessage: string | null = null;
+  loading = false; 
 
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  onSubmit() {
+    this.errorMessage = null;
+    this.loading = true;
+
+    this.authService.login(this.email, this.password).subscribe({
+      next: (res) => {
+        console.log('Login successful:', res);
+        this.loading = false;
+        this.router.navigate(['/']);
+    }, 
+      error: (err) => {
+        this.loading = false;
+        this.errorMessage = 'Login failed. Please check your credentials and try again.';
+      }
+    });
+  }
 }
