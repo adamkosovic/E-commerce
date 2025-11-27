005b7b60ad62facdaba12559bcbfba7a14f55dfa
adamkosovic <adam.kosovic@icloud.com>
2025-11-27 18:45:27 +0100

    Admin: add full product admin CRUD(create, edit, delete) with role-based access

--- frontend/src/app/pages/admin/products/admin-product-list/admin-product-list.ts
+++ frontend/src/app/pages/admin/products/admin-product-list/admin-product-list.ts
@@ -1,22 +1,123 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Observable, map } from 'rxjs';
 import { Product } from '../../../../models/product/product.model';
-import { ProductService } from '../../../../product.service';
+import { ProductService, ProductPayload} from '../../../../product.service';
 import { CommonModule, AsyncPipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-admin-product-list',
   standalone: true,
-  imports: [CommonModule , AsyncPipe],
+  imports: [CommonModule , AsyncPipe, FormsModule],
   templateUrl: './admin-product-list.html',
   styleUrl: './admin-product-list.css'
 })
-export class AdminProductList {
+export class AdminProductList implements OnInit {
   products$!: Observable<Product[]>;
 
-  constructor(private productService: ProductService) {}
+  //state för Formulär
+  isEditing = false;
+  isCreating = false;
+  selectedProductId: string | null = null;
+
+  formModel: ProductPayload = {
+    title: '',
+    description: '',
+    price: 0,
+    imageUrl: ''
+  };
+
+  constructor(private productService: ProductService, private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
-    this.products$ = this.productService.getAll();
+    this.loadProducts();
+  }
+
+  loadProducts(): void {
+    this.products$ = this.productService.getAll().pipe(
+      map(products => [...products].reverse())
+    );
+  }
+
+  // klick på "+ Ny produkt"
+  startCreate(): void {
+    this.isCreating = true;
+    this.isEditing = false;
+    this.selectedProductId = null;
+    this.formModel = {
+      title: '',
+      description: '',
+      price: 0,
+      imageUrl: ''
+    };
+  }
+
+  // klick på "Redigera"
+  startEdit(p: Product): void {
+    this.isEditing = true;
+    this.isCreating = false;
+    this.selectedProductId = p.id;  
+    this.formModel = {
+      title: p.title,
+      description: p.description,
+      price: p.price,
+      imageUrl: p.imageUrl
+    };
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  // klick på "Ta bort"
+  delete(p: Product): void {
+    if (!confirm(`Vill du ta bort "${p.title}"?`)) return;
+
+    this.productService.delete(p.id).subscribe({
+      next: () => {
+        this.loadProducts(),
+        this.cdr.detectChanges();
+      },
+      error: (err) => console.error('Delete failed', err)
+    });
+  }
+
+  // submit på formuläret
+  submitForm(): void {
+    if (this.isCreating) {
+      this.productService.create(this.formModel).subscribe({
+        next: () => {
+          this.loadProducts();
+          this.isCreating = false;
+          this.resetForm();
+          this.cdr.detectChanges();
+        },
+        error: (err) => console.error('Create failed', err)
+      });
+    } else if (this.isEditing && this.selectedProductId) {
+      this.productService.update(this.selectedProductId, this.formModel).subscribe({
+        next: () => {
+          this.loadProducts();
+          this.isEditing = false;
+          this.resetForm();
+          this.cdr.detectChanges();
+        },
+        error: (err) => console.error('Update failed', err)
+      });
+    }
+  }
+
+  cancelForm(): void {
+    this.isEditing = false;
+    this.isCreating = false;
+    this.resetForm();
+  }
+
+  private resetForm(): void {
+    this.selectedProductId = null;
+    this.formModel = {
+      title: '',
+      description: '',
+      price: 0,
+      imageUrl: ''
+    };
   }
 }
--- frontend/src/app/product.service.ts
+++ frontend/src/app/product.service.ts
@@ -1,21 +1,61 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from './models/product/product.model';
+import { environment } from '../environments/enviroment';
 
+
+export interface ProductPayload {
+  title: string;
+  description?: string;
+  price: number;
+  imageUrl: string;
+}
 
 @Injectable({ providedIn: 'root' })
 export class ProductService {
-  private baseUrl = '/api/products';  //proxyn skickar vidare till backend
+  private apiUrl = environment.apiUrl; 
 
   constructor(private http : HttpClient) {}
 
+  // Hjälpmetod för att få med Authorization header om token finns
+  private getAuthHeaders(): HttpHeaders | undefined {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        return undefined;
+      }
+      return new HttpHeaders({
+        Authorization: `Bearer ${token}`
+    });
+  }
+
   getAll(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.baseUrl);
+    return this.http.get<Product[]>(`${this.apiUrl}/products`);
   }
 
   getById(id: string): Observable<Product> {
-    return this.http.get<Product>(`/api/products/${id}`);
+    return this.http.get<Product>(`${this.apiUrl}/products` + `/${id}`);
+  }
+
+  create(payload: ProductPayload): Observable<Product> {
+    const headers = this.getAuthHeaders();
+    return this.http.post<Product>(`${this.apiUrl}/products`, payload, {
+      headers
+    });
+  }
+
+  update(id: string, payload: ProductPayload): Observable<Product> {
+    const headers = this.getAuthHeaders();
+    return this.http.put<Product>(`${this.apiUrl}/products` + `/${id}`, payload, {
+      headers
+    });
+  }
+
+  delete(id: string): Observable<void> {
+    const headers = this.getAuthHeaders();
+    return this.http.delete<void>(`${this.apiUrl}/products` + `/${id}`, {
+      headers
+    });
   }
 
 }
