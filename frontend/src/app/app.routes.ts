dceea171ccd6c2c16e523e63fc0799ad64454454
adamkosovic <adam.kosovic@icloud.com>
2025-11-28 12:12:17 +0100

    Removed code

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModel } from '@angular/forms';
 import { App } from './app';
 import { Home } from './pages/home/home'; // Corrected import path
-import { About } from './pages/about/about'; // Example additional component
 import { Shop } from './pages/shop/shop'; // Example additional component
-import { Stories } from './pages/stories/stories'; // Example additional component
 import { ProductDetailComponent } from './components/product-detail/product-detail';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
@@ -16,8 +14,6 @@ export const routes: Routes = [
   { path: '', component: Home, title: 'Home'},
   { path: 'shop', component: Shop, title: 'Shop' },
   { path: 'shop/:id', component: ProductDetailComponent, title: 'Product Details' },
-  { path: 'stories', component: Stories, title: 'Stories' },
-  { path: 'about', component: About, title: 'About' },
   { path: 'login', component: Login, title: 'Login' },
   { path: 'register', component: Register, title: 'Register' },
   { path: 'admin/products', component: AdminProductList, canActivate: [AdminGuard] , title: 'Admin Products' },
